6c5da6a9e28d5a38af85d3a504d6a72388d66f7a
OMOTOSHO <bankolemelchior@gmail.com>
2023-02-09 18:21:58 +0100

    starting

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int nb = 12788; 
+        System.out.println("Hello world");
+        System.out.println("Starting with java");
+        System.out.println(nb);
+        System.out.print("from print method");
+        System.out.print(" it not go back on new line");
+        System.out.println(false);
+    }
+
+    
+}
+
---
+++ mypack/Mypackage.java
@@ -0,0 +1,8 @@
+package mypack;
+
+public class Mypackage {
+    public static void main(String[] args) {
+        System.out.println("From package");
+    }
+    
+}
