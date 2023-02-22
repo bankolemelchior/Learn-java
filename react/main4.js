8bc9c1c58ddc1ef9b0ee14f0402b4aa11ac56c27
OMOTOSHO <bankolemelchior@gmail.com>
2023-02-22 09:36:37 +0100

    before moving

---
+++ react/main4.js
@@ -0,0 +1,29 @@
+
+
+function FavoriteColor() {
+    const [color, setColor] = useState("red");
+  
+    return (
+      <div>
+        <h1>My favorite color is {color}!</h1>
+        <button
+          type="button"
+          onClick={() => setColor("blue")}
+        >Blue</button>
+        <button
+          type="button"
+          onClick={() => setColor("red")}
+        >Red</button>
+        <button
+          type="button"
+          onClick={() => setColor("pink")}
+        >Pink</button>
+        <button
+          type="button"
+          onClick={() => setColor("green")}
+        >Green</button>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<FavoriteColor />, document.getElementById('root'));
